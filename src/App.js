17c3c60aa5661fb4bf3451d8f53555c9da676f5e
skalo06 <taha.000.kech@gmail.com>
2023-05-15 16:15:16 +0100

    pushing the app for the first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import i18n from "./i18n";
+const App = () => {
+  const { t } = useTranslation();
 
-function App() {
+  const handleLanguageChange = (language) => {
+    i18n.changeLanguage(language);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{t("greeting")}</h1>
+
+      <select onChange={(e) => handleLanguageChange(e.target.value)}>
+        <option value="en">English</option>
+        <option value="ar">Arabic</option>
+        <option value="de">German</option>
+      </select>
     </div>
   );
-}
+};
 
 export default App;
